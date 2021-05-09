ee4b4343fe17f18256028462cb07058bcffc0069
Manan Gouhari <gouhari21@hotmail.com>
2021-05-10 02:48:08 +0530

    MVP for the cryptobot indexer.

---
+++ index.js
@@ -0,0 +1,73 @@
+const { sequelize, Cryptobot, User } = require("./models");
+const { get } = require("axios");
+
+async function main() {
+  sequelize.authenticate();
+  const res = await updateCryptobots();
+  console.log(res.length, "cryptobots added to the DB.");
+}
+
+async function updateCryptobots() {
+  /* 
+    1. Loop over the Cryptobots.
+    2. For every Cryptobot, check if it's in the DB.
+      i. Yes, break.
+      ii. No ->
+        a. Find the user.
+        b. Create the Cryptobot.
+        c. Attach the Cryptobot, to the user.
+  */
+
+  const ADDRESS = "KT1V6cNW5jTUxEwmMhxvNHkMF3Bkm5a9Cfrt";
+  offset = 0;
+  const allCryptobots = [];
+  while (true) {
+    console.log(`Current offset: ${offset}`);
+    const resp = await get(
+      `https://api.better-call.dev/v1/contract/mainnet/${ADDRESS}/tokens?offset=${
+        10 * offset
+      }`
+    );
+
+    const cryptobots = resp.data;
+    if (cryptobots === null || cryptobots?.length == 0) break;
+    allCryptobots.push(...cryptobots);
+    for (cryptobot of cryptobots) {
+      console.log("Indexing cryptobot", cryptobot.token_id);
+
+      let bot = await Cryptobot.findOne({
+        where: { token_id: cryptobot.token_id },
+      });
+
+      if (bot) {
+        console.log(`Bot-${cryptobot.token_id} already is in the DB.`);
+        return;
+      }
+
+      bot = await Cryptobot.create({ token_id: cryptobot.token_id });
+      console.log(`Bot-${bot.token_id} created.`);
+
+      if (!cryptobot?.creators) {
+        console.log("Bot doesn't have a creator.");
+        continue;
+      }
+
+      const user = await User.findOne({
+        where: { xtzAddress: cryptobot.creators[0] },
+      });
+      if (!user) {
+        console.log("User not found.");
+        continue;
+      }
+
+      await user.addCryptobot(bot);
+      console.log("Cryptobot added to user.");
+    }
+    console.log("Increased offset.");
+    offset += 1;
+  }
+
+  return allCryptobots;
+}
+
+main();
---
+++ models/user.js
@@ -0,0 +1,49 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Cryptobot }) {
+      this.hasMany(Cryptobot);
+    }
+
+    toJSON() {
+      return { ...this.get(), id: undefined };
+    }
+  }
+  User.init(
+    {
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      xtzAddress: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      verified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "users",
+    }
+  );
+  return User;
+};
